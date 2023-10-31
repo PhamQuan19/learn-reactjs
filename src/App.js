c24391ce94b82186f557a0d49a5506aa87148f32
Phạm Quân <phamquan29hy@gmail.com>
2023-10-31 22:50:45 +0700

    5_5 TodoList

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name='Quan';
-  const age=22;
-  const isFemal=true;
-  const student={
-    name:'Qtech'
-  };
-
-  const colorList=['red','green','blue'];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pham Quan
-        </p>
-        <p>Xin chao {name} - {age} - {isFemal ? 'Male': 'Female'}</p>
-        <p>{student.name}</p>
-
-        <ul>
-          {colorList.map(color =>(
-            <li style={{color}}>{color}</li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature></TodoFeature>
     </div>
   );
 }
